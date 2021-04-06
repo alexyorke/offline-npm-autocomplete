b08df8908f9a6269279ab777278107042a8345f5
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2021-04-05 23:41:47 -0400

    Use home directory

--- offline-npm-autocomplete-updater.sh
+++ offline-npm-autocomplete-updater.sh
@@ -3,6 +3,6 @@
 # It writes to a temporary file first in-case the file can't be found or the internet goes out, in which case the previous file won't be overwritten.
 # This means that the old file won't be truncated by accident.
 tmpFile="$(mktemp -p /dev/shm)";
-mkdir -p /dev/shm/offline-npm-autocomplete/ && \
+mkdir -p ~/.offline-npm-autocomplete && \
 curl -sL --compressed "https://github.com/alexyorke/all-npm-package-names/raw/main/all_package_names.txt" > "$tmpFile" && \
-mv "$tmpFile" "/dev/shm/offline-npm-autocomplete/all_package_names.txt";
+mv "$tmpFile" "~/.offline-npm-autocomplete/all_package_names.txt";
